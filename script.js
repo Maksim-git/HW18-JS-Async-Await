117045a9364ccc3aa74f65f433c64f89d2a5ada8
Maksim-git <dv.maksim11@gmail.com>
2022-06-18 16:25:39 +0300

    code implementation using  async / await

---
+++ script.js
@@ -0,0 +1,44 @@
+function delay(dataUser, ms) {
+  return new Promise((resolve, reject) => setTimeout(resolve(dataUser), ms));
+}
+
+async function getUserInfo() {
+  return delay({ name: "Vic", age: 21, id: 1 }, 1000);
+}
+
+async function getUserAvatar(userInfo) {
+  await delay(
+    (userInfo.avatar =
+      "https://previews.123rf.com/images/stockgiu/stockgiu1708/stockgiu170802061/83728179-avatar-sketch-of-a-funny-man-s-haed-with-sunglasses-and-hairstyle-design.jpg"),
+    1000
+  );
+}
+
+async function getUserAdditionalInfo(userInfo) {
+  await delay((userInfo.interests = ["sport", "books"]), 1000);
+}
+
+async function getResult() {
+  let objUser = await getUserInfo();
+  await getUserAvatar(objUser);
+  await getUserAdditionalInfo(objUser);
+
+  console.log(objUser);
+}
+
+getResult();
+// -------
+
+async function getUser() {
+  return { name: "Vic", age: 21, id: 1 };
+}
+async function getInfo() {
+  try {
+    let user = await getUser();
+    throw new Error("error");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getInfo();
